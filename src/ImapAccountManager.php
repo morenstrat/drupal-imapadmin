e36fc4fecc3ee9f3c13d6b34986f34a9a12b0598
Markus Orenstrat <markus@dunix-data.de>
2015-12-01 10:59:46 +0100

    Add ImapAccountManager service stub.

---
+++ src/ImapAccountManager.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\imapadmin\ImapAccountManager.
+ */
+
+namespace Drupal\imapadmin;
+
+/**
+ * Implements the ImapAccountManagerInterface.
+ */
+class ImapAccountManager implements ImapAccountManagerInterface {
+}
---
+++ src/ImapAccountManagerInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\imapadmin\ImapAccountManagerInterface.
+ */
+
+namespace Drupal\imapadmin;
+
+/**
+ * Provides an interface for managing IMAP accounts.
+ */
+interface ImapAccountManagerInterface {
+}
