0788e12c4ed0bc326c6ee3d6ca010a33b9a83087
Markus Orenstrat <markus@dunix-data.de>
2015-12-01 10:55:48 +0100

    Rename ImapManager service to ImapMailboxManager.

---
+++ src/ImapMailboxManager.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\imapadmin\ImapMailboxManager.
+ */
+
+namespace Drupal\imapadmin;
+
+/**
+ * Implements the ImapMailboxManagerInterface.
+ */
+class ImapMailboxManager implements ImapMailboxManagerInterface {
+}
---
+++ src/ImapMailboxManagerInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\imapadmin\ImapMailboxManagerInterface.
+ */
+
+namespace Drupal\imapadmin;
+
+/**
+ * Provides an interface for managing IMAP mailboxes.
+ */
+interface ImapMailboxManagerInterface {
+}
